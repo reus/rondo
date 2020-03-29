83a182149498b52bfa70cbbe187ab0d907e05e3f
Ruud Reus <ruud@reus.dev>
2020-03-29 22:06:29 +0200

    * update deprecated reagent.core/render to reagent.dom/render * add comments

--- src/cljs/dev/reus/rondo/app.cljs
+++ src/cljs/dev/reus/rondo/app.cljs
@@ -4,16 +4,19 @@
             [dev.reus.rondo.gamedata]
             [cljs.pprint :refer [pprint]]
             [reagent.core :as reagent]
+            [reagent.dom :as rdom]
             [cljs.core.async :as async :refer [<! >! chan put! timeout]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (enable-console-print!)
 
 (defn print-player-info! [player]
+  "Print information of a player."
   (let [p (select-keys player [:name :pos])]
     (pprint p)))
 
 (defn find-color [team]
+  "Determine the color of given team keyword."
   (loop [i 0]
     (if-let [t (get dev.reus.rondo.gamedata/teams i)]
       (if (= (:id t) team)
@@ -22,6 +25,8 @@
       [0 0 0])))
 
 (defn init-players [players]
+  "Initialize player vector. Adds an index, color and colorstring
+   to each player hashmap."
   (loop [i 0 p-ext []]
     (if-let [player (get players i)]
       (let [team (:team player)
@@ -34,6 +39,7 @@
       p-ext)))
 
 (defn init-state []
+  "Create a state hashmap."
   (let [players dev.reus.rondo.gamedata/players
         ;webgl (dev.reus.rondo.webgl/setup-webgl (count players))]
         drawing-context (dev.reus.rondo.canvas2d/init-drawing-context)]
@@ -50,10 +56,12 @@
      :players (init-players players)}))
 
 (defn render! [state]
+  "Render all game objects to the drawing context."
   ;(dev.reus.rondo.webgl/draw-scene! state))
   (dev.reus.rondo.canvas2d/draw-scene! state))
 
 (defn step [s]
+  "Take a step in the game heartbeat. Increments the framecount, and updates the milliseconds."
   (let [current-millis (.now js/Date)
         dmillis (- current-millis (:current-millis s))
         frame (:frame s)
@@ -63,15 +71,18 @@
     new-state))
 
 (defn print-state [state]
+  "Print game state."
   (let [players (:players state)]
     (doseq [player players]
       (print-player-info! player))
     state))
 
 (defn dot-product [v1 v2]
+  "Helper function to compute the dot product."
   (apply + (map * v1 v2)))
 
 (defn point-in-player? [[x y] state]
+  "Determine whether the current point (x,y) is part of a player's body."
   (let [player-size (:player-size dev.reus.rondo.gamedata/settings)
         ab [(* 2 player-size) 0]
         bc [0 (* 2 player-size)]]
@@ -91,13 +102,16 @@
         false))))
 
 (defn reset-positions [state]
+  "Reset all player's positions."
   state)
 
 (defn reset-position-selected-player [state]
+  "Reset the position of the selected player."
   (let [index (:selected-player state)]
     (assoc-in state [:players index :pos] (:pos (get dev.reus.rondo.gamedata/players index)))))
 
 (defn random-position-selected-player [state]
+  "Change player's position to a random position."
   (let [index (:selected-player state)]
     (loop []
       (let [x (+ 5 (rand-int 391))
@@ -112,6 +126,7 @@
           (assoc-in state [:players index :pos] [x y]))))))
 
 (defn randomize-positions [state]
+  "Randomize all player's positions"
   (let [players (loop [i 0 ps (:players state) positions #{}]
                   (if-let [p (get ps i)]
                     (let [x (+ 5 (* 10 (rand-int 40)))
@@ -123,6 +138,11 @@
     (assoc state :players players)))
 
 (defn start-game-loop [state ui-channel r-atom worker] 
+  "Start the game loop. Starts a go loop that listens for three channels:
+   * refresh channel: redraws the game world.
+   * ui channel: listens for input from user.
+   * player channel: listen for input from player entities.
+   Returns the channel through which players can communicate."
   (let [player-channel (chan 100)
         rate (:refresh-rate state)]
     (render! state)
@@ -140,29 +160,34 @@
                                (recur refresh new-state))))))
     player-channel))
 
-(defn ui-select-team [state team]
-    (assoc state :selected-team team :selected-player nil))
+(defn ui-select-team [state t]
+  "Set team t as selected in game state."
+    (assoc state :selected-team t :selected-player nil))
 
 (defn ui-select-player [state player-index]
+  "Set player with index player-index as selected in game state."
     (assoc state :selected-team nil :selected-player player-index))
 
 (defn ui-player [state ui-chan]
+  "Returns Reagent array for interface handling players."
   (let [player-index (:selected-player state)
         player (get (:players state) player-index)
         return (fn [f] (fn [] (put! ui-chan f)))]
     [:div
-     [:a {:on-click (return (fn [s] (assoc s :selected-player nil)))} "Back"]
+     [:a {:class "back ":on-click (return (fn [s] (assoc s :selected-player nil)))} "Back"]
      [:div "name: " (:name player)]
      [:div [:input {:type "button" :value "Reset position" :on-click (return (fn [s] (reset-position-selected-player s)))}]]
      [:div [:input {:type "button" :value "Random position" :on-click (return (fn [s] (random-position-selected-player s)))}]]
      ]))
 
 (defn ui-team [state ui-chan]
+  "Returns Reagent array for interface handling teams."
   (let [return (fn [f] (fn [] (put! ui-chan f)))]
     [:div
-     [:a {:on-click (return (fn [s] (assoc s :selected-team nil)))} "Back"]]))
+     [:a {:class "back" :on-click (return (fn [s] (assoc s :selected-team nil)))} "Back"]]))
 
-(defn draw-ui [state-atom ui-chan]
+(defn get-ui [state-atom ui-chan]
+  "Get the Reagent array that forms the user interface."
   (let [return (fn [f] (fn [] (put! ui-chan f)))
         state @state-atom]
     [:div
@@ -192,6 +217,7 @@
             "General settings"))]]))
 
 (defn process-mousedown [state event]
+  "Process a mousedown event in the canvas."
   (let [canvas (.-target event)
         rect (.getBoundingClientRect canvas)
         x (- (.-clientX event) (.-left rect))
@@ -205,30 +231,36 @@
       state)))
 
 (defn process-keydown [state event]
+  "Process a keydown event in the canvas."
   (.log js/console "test")
   state)
 
-(defn setup-canvas-handlers [ui-chan]
+(defn setup-event-handlers! [ui-chan]
+  "Add event listeners."
   (let [canvas (by-id "canvas")]
-    (.addEventListener canvas "mousedown" (fn [e] (put! ui-chan (fn [s] (process-mousedown s e)))))))
-
-(defn setup-key-handlers [ui-chan]
-  (.addEventListener js/document "keydown" (fn [e] (put! ui-chan (fn [s] (process-keydown s e))))))
+    (.addEventListener canvas "mousedown" (fn [e] (put! ui-chan (fn [s] (process-mousedown s e)))))
+    (.addEventListener js/document "keydown" (fn [e] (put! ui-chan (fn [s] (process-keydown s e)))))))
 
 (defn setup-ui [state]
+  "Setup the main user interface rendered by Reagent. Creates a core async channel through that offers
+   a means to communicate with the game loop. Returns the channel and a reagent atom with ui state."
   (let [ui-chan (chan)
         reagent-state (reagent/atom state)
-        ui (fn [] (draw-ui reagent-state ui-chan))]
+        ui (fn [] (get-ui reagent-state ui-chan))]
     (go (loop []
           (<! (timeout 1000))
           (>! ui-chan identity)
           (recur)))
-    (setup-canvas-handlers ui-chan)
-    (setup-key-handlers ui-chan)
-    (reagent/render [ui] (by-id "ui"))
+    (setup-event-handlers! ui-chan)
+    (rdom/render [ui] (by-id "ui"))
     [ui-chan reagent-state]))
 
 (defn start-game []
+  "Starts the game.
+   * Initializes game state
+   * Sets up user interface
+   * Runs the webworker script
+   * Starts the game loop and binds the webworker code to the player channel." 
   (let [state (init-state)
         [ui-channel r-atom] (setup-ui state)
         worker (js/Worker. "/worker.js")
@@ -240,6 +272,7 @@
     ;(.postMessage worker state)
     1))
 
-(defonce game (start-game))
+(defn on-js-reload []
+  "Figwheel convenience function to change your state on js reload.")
 
-(defn on-js-reload [])
+(defonce game (start-game))
